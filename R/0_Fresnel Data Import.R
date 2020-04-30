a33fd6e124cfa94001e5e8fba461bb9408588f77
Greb <gfalbery@gmail.com>
2020-04-30 13:31:41 -0400

    Adding Farrell

--- R/0_Fresnel Data Import.R
+++ R/0_Fresnel Data Import.R
@@ -1,7 +1,7 @@
 
 # Fresnel Data Import ####
 
-library(tidyverse); library(data.table); library(fs)
+library(tidyverse); library(data.table); library(fs); library(ggregplot)
 
 if(file.exists("Intermediate/AlberyPredicted.rds")){
   
@@ -14,14 +14,52 @@ if(file.exists("Intermediate/AlberyPredicted.rds")){
   
 }
 
-
 CarlsonPredicted <- fread("Data/CarlsonPredicted.csv")
 
+CarlsonPredicted %>% rename_all(CamelConvert) %>% 
+  mutate_at("Host_species", ~.x %>% str_trim %>% str_replace_all(" ", "_")) ->
+  
+  CarlsonPredicted
+
+
+# 03_Damas Predictions
+# Adding the Damas dataset for ####
+# https://www.biorxiv.org/content/10.1101/2020.04.16.045302v1.supplementary-material
+
+Damas <- read.csv("Data/Damas_Ace2.csv", header = T)
+
+Damas %>% 
+  rename(DamasRank = Sort.by.predicted.susceptibility) %>%
+  arrange(DamasRank) -> DamasRank
+
+DamasRank %>% 
+  mutate_at("Species", ~.x %>% str_trim %>% str_replace_all(" ", "_")) ->
+  
+  Damas
 
 BeckerPredicted <- fread("Data/PhylofactorPredicted.csv")
 
 BeckerPredicted %>% arrange(desc(preds_allbetacov)) %>%
   mutate(BeckerRank = rank(preds_allbetacov)) %>% 
-  mutate_at("BeckerRank", ~max(.x) - .x + 1) ->
+  mutate_at("BeckerRank", ~max(.x) - .x + 1) %>% rename(Sp = V1) ->
   
   BeckerPredicted
+
+PoisotPredicted <- fread("Data/PoisotPredicted.csv")
+
+PoisotPredicted %>%
+  rename_all(CamelConvert) %>%
+  #filter(Virus %>% str_detect("Beta")) %>%
+  mutate_at("Host", ~.x %>% str_trim %>% str_replace_all(" ", "_")) %>%
+  rename(PoisotRank = Match) -> 
+  
+  PoisotPredicted
+
+FarrellPredicted <- 
+  #read_csv("https://github.com/ViromeNet/farrell-betacov/blob/master/results/batcov_elmasri_phylo_pred_betacovsOnly.csv")
+  read_csv("https://raw.githubusercontent.com/ViromeNet/farrell-betacov/master/results/batcov_elmasri_phylo_pred_betacovsOnly.csv?token=ADQWUG7DMRONOQL4HC24BSC6VMEKU")
+
+#FarrellPredicted %>% write.csv("Data/FarrellPredicted.csv")
+
+FarrellPredicted %>% rename(FarrellRank = rank) ->
+  FarrellPredicted
--- R/X_Comparison Output.R
+++ R/X_Comparison Output.R
@@ -1,42 +1,53 @@
 
 # X_Comparison ####
 
-library(colorspace)
+library(colorspace); library(cowplot)
+
+theme_set(theme_cowplot())
 
 AlberyPredictedBats %>% 
   rename(AlberyRank = Rank) %>%
   full_join(Damas, by = c("Sp" = "Species")) %>%
   full_join(CarlsonPredicted %>% rename(CarlsonRank = Rank), 
             by = c("Sp" = "Host_species")) %>%
-  mutate_at("DamasRank", ~ifelse(is.na(.x), "Unknown", .x) %>% factor(levels = c("Unknown", "5", "4"))) ->
+  mutate_at("DamasRank", ~ifelse(is.na(.x), "Unknown", .x) %>%
+              factor(levels = c("Unknown", "5", "4"))) %>% 
+  full_join(FarrellPredicted, by = c("Sp" = "Host")) %>% 
+  full_join(PoisotPredicted, by = c("Sp" = "Host")) %>% 
+  full_join(BeckerPredicted, by = c("Sp")) ->
   
   FullPredictions
 
-FullPredictions %>% 
-  filter(hOrder == "Chiroptera") %>%
-  select(AlberyRank, CarlsonRank, DamasRank) %>%
-  mutate_at("AlberyRank", GregCube) %>%
-  mutate_all(~1 - .x) %>%
-  GGally::ggpairs(lower = list(continuous = "smooth"))
+FullPredictions %>% mutate(FarrellBinary = as.numeric(!is.na(FarrellRank))) ->
+  
+  FullPredictions
 
-FullPredictions %>% 
-  filter(hOrder == "Chiroptera") %>%
-  ggplot(aes(GregCube(AlberyRank), DamasRank)) + 
-  geom_point() + ggpubr::stat_cor() + 
-  labs(x = expression(AlberyRank^{3})) +
-  geom_smooth(method = lm)
-
-
-FullPredictions %>% 
-  filter(hOrder == "Chiroptera") %>%
-  ggplot(aes(GregCube(AlberyRank), DamasRank)) + 
-  geom_point(aes(colour = DamasRank)) + 
-  ggpubr::stat_cor() + 
-  labs(x = expression(AlberyRank^{3})) +
-  geom_smooth(method = lm)
+VarNames <- paste0(c("Albery", "Carlson", "Damas", "Farrell", "Poisot", "Becker"), "Rank")
 
 GregLimit <- 7
 ColinLimit <- 300
+
+FullPredictions %>% 
+  filter(hOrder == "Chiroptera") %>%
+  select(all_of(VarNames)) %>%
+  mutate_at("AlberyRank", GregCube) %>%
+  #mutate_all(~1 - .x) %>%
+  GGally::ggpairs(lower = list(continuous = "smooth"))
+
+
+FullPredictions %>% 
+  filter(hOrder == "Chiroptera") %>%
+  ggplot(aes(GregCube(AlberyRank), as.numeric(DamasRank))) + 
+  geom_point() + ggpubr::stat_cor() + 
+  labs(x = expression(AlberyRank^{3})) +
+  geom_smooth()
+
+FullPredictions %>% 
+  filter(hOrder == "Chiroptera") %>%
+  ggplot(aes(GregCube(AlberyRank), CarlsonRank)) + 
+  geom_point() + ggpubr::stat_cor() + 
+  labs(x = expression(AlberyRank^{3})) +
+  geom_smooth(colour = AlberColours[[1]])
 
 (OutsetPredictionPlot <- FullPredictions %>% 
   filter(hOrder == "Chiroptera") %>%
@@ -69,18 +80,16 @@ ColinLimit <- 300
   scale_size_manual(values = c(0.6, 0.8, 1)*5) + 
   geom_text(aes(label = Sp, size = DamasRank, alpha = DamasRank, colour = as.factor(Betacov)))) %>% plot
 
-OutsetPredictionPlot + ggsave("Shot.jpeg")
-InsetPredictionPlot + ggsave("Chaser.jpeg")
-
-(OutsetPredictionPlot/InsetPredictionPlot) +
-  ggsave()
+# OutsetPredictionPlot + ggsave("Shot.jpeg")
+# InsetPredictionPlot + ggsave("Chaser.jpeg")
 
 AddPreds <- c("Cynopterus sphinx",
               "Taphozous melanopogon",
               "Hipposideros lekaguli",
               "Rhinolophus shameli",
               "Scotophilus heathii",
-              "Megaderma lyra") %>% str_replace_all(" ", "_")
+              "Megaderma lyra") %>% 
+  str_replace_all(" ", "_")
 
 (FullPredictions %>% 
   filter(hOrder == "Chiroptera") %>%
@@ -104,11 +113,6 @@ AddPreds <- c("Cynopterus sphinx",
 
 # Adding Phylofactor ####
 
-FullPredictions %>% full_join(BeckerPredicted %>% rename(Sp = V1)) ->
-  
-  FullPredictions
-
-
 (FullPredictions %>% 
     filter(hOrder == "Chiroptera") %>%
     ggplot(aes(GregCube(AlberyRank), CarlsonRank)) + 
@@ -130,3 +134,52 @@ FullPredictions %>% full_join(BeckerPredicted %>% rename(Sp = V1)) ->
      facet_wrap(~BeckerRank) +
     scale_colour_manual(values = c(AlberColours[[1]], AlberColours[[3]]))) %>% plot
 
+
+# Adding Poisot ####
+
+(FullPredictions %>% 
+   filter(hOrder == "Chiroptera") %>%
+   ggplot(aes(GregCube(AlberyRank), CarlsonRank)) + 
+   
+   geom_rect(xmin = -Inf, ymin = -Inf, xmax = GregLimit, ymax = ColinLimit,
+             fill = NA, lty = 2, alpha = 0.3, colour = "grey") +
+   
+   geom_point(aes(size = DamasRank, 
+                  alpha = PoisotRank, 
+                  shape = as.factor(BeckerRank),
+                  colour = as.factor(Betacov))) +
+   
+   ggpubr::stat_cor() + 
+   labs(x = expression(AlberyRank^{3})) +
+   geom_smooth(method = lm, colour = AlberColours[[1]], fill = NA) +
+   stat_smooth(geom = "ribbon", method = lm, 
+               colour = AlberColours[[1]], fill = NA, lty = 2) +
+   # scale_x_continuous(breaks = c(1:16), labels = c(1:16)^3) +
+   # scale_alpha_manual(values = c(0.6, 0.8, 1)) + 
+   facet_wrap(~is.na(PoisotRank)) +
+   scale_colour_manual(values = c(AlberColours[[1]], AlberColours[[3]]))) %>% plot
+
+
+# Adding Farrell ####
+
+(FullPredictions %>% 
+   filter(hOrder == "Chiroptera") %>%
+   ggplot(aes(GregCube(AlberyRank), CarlsonRank)) + 
+   
+   geom_rect(xmin = -Inf, ymin = -Inf, xmax = GregLimit, ymax = ColinLimit,
+             fill = NA, lty = 2, alpha = 0.3, colour = "grey") +
+   
+   geom_point(aes(size = DamasRank, 
+                  alpha = PoisotRank, 
+                  shape = as.factor(BeckerRank),
+                  colour = as.factor(Betacov))) +
+   
+   ggpubr::stat_cor() + 
+   labs(x = expression(AlberyRank^{3})) +
+   geom_smooth(method = lm, colour = AlberColours[[1]], fill = NA) +
+   stat_smooth(geom = "ribbon", method = lm, 
+               colour = AlberColours[[1]], fill = NA, lty = 2) +
+   # scale_x_continuous(breaks = c(1:16), labels = c(1:16)^3) +
+   # scale_alpha_manual(values = c(0.6, 0.8, 1)) + 
+   facet_wrap(~FarrellBinary) +
+   scale_colour_manual(values = c(AlberColours[[1]], AlberColours[[3]]))) %>% plot
