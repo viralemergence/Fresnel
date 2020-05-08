e4543545fb47278a500e4b7a4b29ec4b486bf2db
Greb <gfalbery@gmail.com>
2020-05-08 13:51:00 -0400

    Cleaning up code

--- R/02_Carlson Predictions.R
+++
@@ -1,7 +0,0 @@
-
-# 2_Carlson Predictions #####
-
-CarlsonPredicted %>% rename_all(CamelConvert) %>% 
-  mutate_at("Host_species", ~.x %>% str_trim %>% str_replace_all(" ", "_")) ->
-  
-  CarlsonPredicted
