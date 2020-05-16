1b412d10623c26fb6139976ceecf4607e29eb4d4
Greb <gfalbery@gmail.com>
2020-05-16 18:48:15 -0400

    Greg Prepping

---
+++ R/00_Fresnel Master.R
@@ -0,0 +1,33 @@
+
+# 00_Fresnel Master Code ####
+
+
+Repos <- c(
+  "albery-betacov",
+  "becker-betacov",
+  "carlson-batcov",
+  "dallas-betacov",
+  "farrell-betacov",
+  "guth-betacov",
+  "poisot-betacov")
+
+
+
+# 0_Sourcing Models ####
+
+
+# 1_Uniting Predictions ####
+
+
+
+
+# 2_Making Bat Figures ####
+
+
+
+# 3_Making Mammal Figures ####
+
+
+
+# 4_Making Maps ####
+
