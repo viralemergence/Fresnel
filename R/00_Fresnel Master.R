d7bfeef93df06b036ad399f38d9b647e4ea97582
Greb <gfalbery@gmail.com>
2020-05-16 19:38:38 -0400

    Fixing carlson betacov name

--- R/00_Fresnel Master.R
+++ R/00_Fresnel Master.R
@@ -1,19 +1,44 @@
 
 # 00_Fresnel Master Code ####
 
+library(tidyverse); library(fs)
 
 Repos <- c(
   "albery-betacov",
-  "becker-betacov",
-  "carlson-batcov",
+  # "becker-betacov",
+  "carlson-betacov",
   "dallas-betacov",
   "farrell-betacov",
   "guth-betacov",
   "poisot-betacov")
 
+SourceScripts <- list(
+  
+  "R/01_Albery Predictions.R",
+  #
+  "BART-1.R",
+  "models/Dallas2017.rmd",
+  "scripts",
+  "",
+  NA
+  
+)
+
+names(SourceScripts) <- Repos
 
 
 # 0_Sourcing Models ####
+
+r <- 1
+
+for(r in seq_along(Repos)){
+  
+  setwd(paste0("Github/Repos/", Repos[r]))
+  
+  source(SourceScripts[[r]])
+  
+  
+}
 
 
 # 1_Uniting Predictions ####
