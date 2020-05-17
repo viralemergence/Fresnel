5c907ab833d6d0791c7ef4395eec16dd3893e513
Greb <gfalbery@gmail.com>
2020-05-17 19:47:54 -0400

    Fixing mammal figures

--- R/03b_Mammal Figures.R
+++ R/03b_Mammal Figures.R
@@ -1,7 +1,7 @@
 
 # Fresnel Figures ####
 
-library(tidyverse); library(cowplot); library(colorspace); library(patchwork); library(ggpubr)
+library(tidyverse); library(cowplot); library(colorspace); library(patchwork); library(ggpubr); library(ggregplot)
 
 theme_set(theme_cowplot() + theme(strip.background = element_rect(fill = "white")))
 
@@ -9,33 +9,19 @@ AlberPalettes <- c("YlGnBu","Reds","BuPu", "PiYG")
 
 AlberColours <- sapply(AlberPalettes, function(a) RColorBrewer::brewer.pal(5, a)[4])
 
-Relabel <- c(
+AlberColours[length(AlberColours)+1:2] <- 
   
-  "Network-based.1", 
-  "Network-based.2",
-  "Phylogenetic.1",
-  "Phylogenetic.2",
-  "Trait-based.1",
-  "Trait-based.2",
-  "Network-based.3"
-  
-)
+  RColorBrewer::brewer.pal(11, AlberPalettes[[4]])[c(2,10)]
 
-Relabel <- c(
-  
-  "Network.1", 
-  "Network.2",
-  "Phylog.1",
-  "Phylog.2",
-  "Trait.1",
-  "Trait.2",
-  "Network.3"
-  
-)
+AlberColours <- c(AlberColours, Pink = "#FD6396", Blue = "#3C78D8")
+
+Relabel <- c(glue::glue("Network.{1:4}"), 
+             glue::glue("Trait.{1:3}"))
 
 names(Relabel) <- c("R.Po2", "R.Po3",
-                    "R.Alb", "R.Far1",
-                    "R.Gut1", "R.Car3", "R.Dal1")
+                    "R.Dal1","R.Far1",
+                    "R.Gut1", "R.Car3",
+                    "R.Alb")
 
 Relabel[intersect(names(Relabel), names(NonBatModels_IS))] ->
   
@@ -46,16 +32,20 @@ Relabel[intersect(names(Relabel), names(NonBatModels_IS))] ->
 NonBatModels_IS %>% 
   gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
   #filter(str_detect(Key, "Alb|Car|Dal|Gut")) %>%
-  #mutate_at("Key", ~.x %>% recode(!!!Relabel)) %>%
+  mutate_at("Key", ~.x %>% recode(!!!Relabel)) %>%
   ggplot(aes(Value, Betacov)) + 
   geom_point(alpha = 0.3, colour = AlberColours[[3]]) + 
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
   #geom_smooth(method = lm, fill = NA, colour = "black") +
-  geom_smooth(fill = NA, colour = "black") +
+  #geom_smooth(fill = NA, colour = "black") +
   facet_wrap(~Key, nrow = 2) +
-  stat_cor(label.y = 1.2) +
-  lims(y = c(-0.2, 1.2)) + coord_fixed() +
+  stat_cor(label.y = 1.2,
+           aes(label = ..rr.label..)) +
+  #coord_fixed() +
   scale_x_continuous(breaks = c(0, 0.5, 1.3)) +
-  scale_y_continuous(breaks = c(0:5/5)) +
+  scale_y_continuous(breaks = c(0:5/5), limits = c(-0, 1.25)) +
   labs(x = "Proportional rank") -> 
   
   SingleCorrelations
@@ -64,27 +54,28 @@ NonBatModels_IS %>%
   mutate(Key = "Multi-model ensemble") %>%
   ggplot(aes(PropRank, Betacov)) + 
   #ggtitle("Model assemblage") +
-  geom_point(alpha = 0.6, colour = AlberColours[[3]], position = position_jitter(h = 0.1)) + 
+  geom_point(alpha = 0.6, colour = AlberColours[[3]], 
+             position = position_jitter(h = 0.05)) + 
   #geom_smooth(method = lm, fill = NA, colour = "black") +
-  geom_smooth(fill = NA, colour = "black") +
-  coord_fixed() + 
-  lims(x = c(0, 1), 
-       y = c(-0.2, 1.25)) +
-  scale_y_continuous(breaks = c(0:5/5)) +
-  stat_cor(label.y = 1.2, method = "spearman") +
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
+  # geom_smooth(fill = NA, colour = "black") +
+  #coord_fixed() + 
+  lims(x = c(0, 1)) +
+  scale_y_continuous(breaks = c(0:5/5), 
+                     limits = c(-0.1, 1.25)) +
+  stat_cor(label.y = 1.2, method = "spearman",
+           aes(label = ..rr.label..)) +
   facet_wrap(~Key) +
   labs(x = "Proportional rank") ->
   
   OverallCorrelations
 
 (SingleCorrelations|OverallCorrelations) + 
-  ggsave("Figures/MammalFigures/Obs_Pred_CorrelationsHorizontal.jpeg", 
-         units = "mm", width = 250, height = 150)
-
-(OverallCorrelations/(SingleCorrelations + facet_wrap(~Key, nrow = 2))) + 
-  plot_layout(heights = c(1, 1.1)) +
-  ggsave("Figures/MammalFigures/Obs_Pred_CorrelationsVertical.jpeg", 
-         units = "mm", width = 150, height = 250)
+  plot_layout(widths = c(1.35,1)) +
+  ggsave("Figures/MammalObs_Pred_CorrelationsHorizontal.jpeg", 
+         units = "mm", width = 325, height = 150)
 
 # Figure 2: Inter-Model Agreement ####
 
@@ -100,6 +91,8 @@ NonBatModels_IS %>%
   
   CorDF_IS %>% rowSums %>% sort %>% rev %>% names -> ModelLimits
   
+  Relabel -> ModelLimits
+  
   CorDF_IS %>% reshape2::melt() %>% #slice(which(lower.tri(CorDF_IS)))
     
     mutate_at("Var1", ~factor(.x, levels = c(ModelLimits))) %>%
@@ -112,7 +105,10 @@ NonBatModels_IS %>%
     theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
     
     # scale_fill_continuous_diverging(palette = "Tropic", limits = c(-1, 1)) +
-    scale_fill_gradient2(low = AlberColours[[1]], mid = "white", high = AlberColours[[3]],
+    scale_fill_gradient2(low = AlberColours[[1]], 
+                         mid = "white", 
+                         high = AlberColours[[5]],
+                         # high = AlberColours[["Pink"]],
                          midpoint = 0, limits = c(-1, 1)) +
     
     labs(x = NULL, y = NULL, fill = "Correlation") +
@@ -123,7 +119,11 @@ NonBatModels_IS %>%
   
   # Bump and agreements ####
   
-  CorDF_IS %>% rowSums %>% sort %>% rev %>% names -> ModelLimits
+  CorDF_IS %>% rowSums %>% #sort %>% 
+    #rev %>% 
+    names -> ModelLimits
+  
+  Relabel -> ModelLimits
   
   NonBatModels_IS %>%
     rename_all(~ifelse(.x %in% names(Relabel), recode(.x, !!!Relabel), .x)) %>% 
@@ -167,23 +167,21 @@ NonBatModels_IS %>%
     labs(x = "Model", y = "Proportional rank", colour = "Top 10 unknown hosts") ->
     
     BumpPlot_IS
-  
+
 }
 
 (TilePlot_IS/
-    
-    (BumpPlot_IS + 
-       #theme(legend.position = "bottom") +
-       #guides(colour = guide_legend(direction = "vertical",
-      #                              ncol = 1,
-      #                              nrow = 10)) +
-       NULL
-    )
-) +
+  
+  (BumpPlot_IS + 
+     theme(legend.position = "bottom") +
+     guides(colour = guide_legend(direction = "vertical",
+                                  hjust = 0.5,
+                                  ncol = 1,
+                                  nrow = 10)))) +
   
   # plot_layout(heights = c(1, 1)) +
   plot_annotation(tag_levels = "A") +
-  ggsave("Figures/MammalFigures/In_Out_ModelAgreement.jpeg", units = "mm", height = 250, width = 200)
+  ggsave("Figures/MammalIn_Out_ModelAgreement.jpeg", units = "mm", height = 250, width = 125)
 
 
 # Auxiliary plots ####
@@ -192,7 +190,7 @@ NonBatModels_IS %>%
 
 NonBatModels_IS %>% 
   gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
-  filter(str_detect(Key, "Alb|Car|Dal|Gut")) %>%
+  #filter(str_detect(Key, "Alb|Car|Dal|Gut")) %>%
   mutate_at("Key", ~.x %>% recode(!!!Relabel)) %>%
   ggplot(aes(Value)) + 
   geom_density(data = NonBatModels_IS, inherit.aes = F, aes(x = PropRank), colour = "black", size = 3) +
