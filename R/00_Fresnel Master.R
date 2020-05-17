5c4b6eaadf69a1f0d3a6848234e810698afe6c51
Greb <gfalbery@gmail.com>
2020-05-16 20:56:22 -0400

    Has a complex purl and copy across rmd to R

--- R/00_Fresnel Master.R
+++ R/00_Fresnel Master.R
@@ -9,6 +9,16 @@ here::here() %>% setwd()
 
 getwd()
 
+# function from https://stackoverflow.com/questions/10966109/how-to-source-r-markdown-file-like-sourcemyfile-r
+
+ksource <- function(x, ...) {
+  
+  library(knitr)
+  
+  source(purl(x, output = tempfile()), ...)
+  
+}
+
 Repos <- c(
   "albery-betacov",
   # "becker-betacov",
@@ -23,7 +33,7 @@ SourceScripts <- list(
   "R/01_Albery Predictions.R",
   #
   "BART-1.R",
-  "models/Dallas2017.rmd",
+  "models",
   "scripts",
   "",
   NA
@@ -34,7 +44,7 @@ names(SourceScripts) <- Repos
 
 # 0_Sourcing Models ####
 
-r <- 2
+r <- 4
 
 for(r in seq_along(Repos)){
   
@@ -42,9 +52,53 @@ for(r in seq_along(Repos)){
   
   setwd(paste0(here::here(),"/Github/Repos/", Repos[r]))
   
-  source(SourceScripts[[r]])
-  
-  
+  if(SourceScripts %>% str_detect("[.]R$|[.]rmd$")){
+    
+    source(SourceScripts[[r]])
+    
+  }else{
+    
+    SourceScripts[[r]] %>% list.files(full.names = T, 
+                                      pattern = "[.]R$|[.]rmd$|[.]Rmd$") ->
+      
+      SubSources
+    
+    file_copy(
+      paste0(here::here(), "/Github/Repos/virionette/04_predictors/bat-supertree_clean.rds"),
+      paste0(SourceScripts[[r]], "/bat-supertree_clean.rds"),
+      overwrite = T
+    )
+    
+    file_copy(
+      paste0(here::here(), "/Github/Repos/virionette/03_interaction_data/virionette.csv"),
+      paste0(SourceScripts[[r]], "/virionette.csv"),
+      overwrite = T
+    )
+    
+    rr <- 1
+    
+    for(rr in seq_along(SubSources)){
+      
+      print(SubSources[[rr]])
+      
+      purl(SubSources[[rr]], 
+           output = paste0(SourceScripts[[r]]))#, "/", SubSources[[rr]]))
+      
+      purl(SubSources[[rr]], 
+           output = paste0(SourceScripts[[r]], "/", (SubSources[[rr]]) %>% 
+             str_split("/") %>% map_chr(2)) %>%
+             str_replace_all(c(".Rmd" = ".R",
+                               ".rmd" = ".R")))
+      
+      SubSources[[rr]] %>% 
+        str_replace_all(c(".Rmd" = ".R",
+                          ".rmd" = ".R")) %>%
+        source
+      
+      ksource(SubSources[[rr]])
+      
+    }
+  }
 }
 
 
