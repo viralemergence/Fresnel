e74d1a2b5226e8b6a3c385f8a4fd4b949dababcd
Greb <gfalbery@gmail.com>
2020-05-24 14:33:56 -0400

    Adding source files for repo downloads

--- R/00_Fresnel Master.R
+++ R/00_Fresnel Master.R
@@ -48,84 +48,12 @@ SourceScripts <- list(
 
 names(SourceScripts) <- Repos
 
-# Master Files ####
+# 0a_Downloading Data Repos ####
 
-MasterFiles <- T
 
-if(MasterFiles){
-  
-  download.file(url = paste0("https://github.com/viralemergence/", "virionette", "/archive/master.zip"),
-                destfile = paste0(here::here(), "/Github/Repos/", "virionette", ".zip"))
-  
-  unzip(paste0(here::here(), "/Github/Repos/", "virionette", ".zip"),
-        exdir = paste0(here::here(), "/Github/Repos/"))
-  
-  Virionette <- read.csv("Github/Repos/virionette-master/03_interaction_data/virionette.csv")
-  Citations <- read.csv("Github/Repos/virionette-master/04_predictors/Citations.csv")
-  HanTraits <- read.csv("Github/Repos/virionette-master/04_predictors/Han-BatTraits.csv")
-  
-  BatTree <- readRDS("Github/Repos/virionette-master/04_predictors/bat-supertree_clean.rds")
-  SuperTree <- readRDS("Github/Repos/virionette-master/04_predictors/Full Supertree.rds")
-  
-  file.rename(paste0(here::here(), "/Github/Repos/", "virionette", "-master"),
-              paste0(here::here(), "/Github/Repos/", "virionette"))
-  
-  file_delete(paste0(here::here(), "/Github/Repos/", "virionette", ".zip"))
-  
-  download.file(url = paste0("https://github.com/viralemergence/", "becker-betacov", "/archive/master.zip"),
-                destfile = paste0(here::here(), "/Github/Repos/", "becker-betacov", ".zip"))
-  
-  unzip(paste0(here::here(), "/Github/Repos/", "becker-betacov", ".zip"),
-        exdir = paste0(here::here(), "/Github/Repos/"))
-  
-  file.rename(paste0(here::here(), "/Github/Repos/", "becker-betacov", "-master"),
-              paste0(here::here(), "/Github/Repos/", "becker-betacov"))
-  
-  file_delete(paste0(here::here(), "/Github/Repos/", "becker-betacov", ".zip"))
-  
-}
+# 0b_Downloading Model Repos ####
 
-# 0_Downloading repos ####
-
-Download <- F
-
-if(Download){
-  
-  r <- 1
-  
-  for(r in r:length(Repos)){
-    
-    FocalRepo <- Repos[[r]] 
-    
-    if(dir.exists(paste0(here::here(), "/Github/Repos/", FocalRepo))){
-      
-      dir_delete(paste0(here::here(), "/Github/Repos/", FocalRepo))
-      
-    }
-    
-    download.file(url = paste0("https://github.com/viralemergence/", FocalRepo, "/archive/master.zip"),
-                  destfile = paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))
-    
-    unzip(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"),
-          exdir = paste0(here::here(), "/Github/Repos"))
-    
-    file.rename(paste0(here::here(), "/Github/Repos/", FocalRepo, "-master"),
-                paste0(here::here(), "/Github/Repos/", FocalRepo))
-    
-  }
-  
-  for(r in r:length(Repos)){
-    
-    FocalRepo <- Repos[[r]] 
-    
-    if(dir.exists(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))){
-      
-      
-      file_delete(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))
-      
-    }
-  }
-}
+source("R/00b_Downloading Model Repos.R")
 
 # 0_Sourcing Models ####
 
---
+++ R/00b_Downloading Model Repos.R
@@ -0,0 +1,42 @@
+
+# 00b_Downloading Model Repos ####
+
+library(tidyverse); library(fs); library(glue); library(zip)
+library(conflicted)
+
+conflict_prefer("unzip", "zip")
+
+r <- 1
+
+for(r in r:length(Repos)){
+  
+  FocalRepo <- Repos[[r]] 
+  
+  if(dir.exists(paste0(here::here(), "/Github/Repos/", FocalRepo))){
+    
+    dir_delete(paste0(here::here(), "/Github/Repos/", FocalRepo))
+    
+  }
+  
+  download.file(url = paste0("https://github.com/viralemergence/", FocalRepo, "/archive/master.zip"),
+                destfile = paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))
+  
+  unzip(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"),
+        exdir = paste0(here::here(), "/Github/Repos"))
+  
+  file.rename(paste0(here::here(), "/Github/Repos/", FocalRepo, "-master"),
+              paste0(here::here(), "/Github/Repos/", FocalRepo))
+  
+}
+
+for(r in r:length(Repos)){
+  
+  FocalRepo <- Repos[[r]] 
+  
+  if(dir.exists(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))){
+    
+    
+    file_delete(paste0(here::here(), "/Github/Repos/", FocalRepo, ".zip"))
+    
+  }
+}
