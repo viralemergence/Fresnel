d02b7e56e45d70ca130984ec85ff4bc8354eb25a
Colin J. Carlson <cjcarlson@berkeley.edu>
2020-06-22 16:55:30 -0400

    thresholder

--- R/X_Thresholder.R
+++ R/X_Thresholder.R
@@ -31,15 +31,15 @@ tvalues <- optimal.thresholds(data.frame(BatModels2[,c('n','Betacov',RNames,PNam
                               req.sens = 0.9,
                               na.rm = TRUE)
 
-for (name in RNames) {BatModels2[,name] <- (BatModels2[,name] > tvalues[1,name])}
-for (name in PNames) {BatModels2[,name] <- (BatModels2[,name] > tvalues[1,name])}
+for (name in RNames) {BatModels2[,name] <- as.vector(BatModels2[,name] > tvalues[1,name])}
+for (name in PNames) {BatModels2[,name] <- as.vector(BatModels2[,name] > tvalues[1,name])}
 
 colSums(BatModels2[BatModels2$Betacov==0,RNames], na.rm = TRUE)
 colSums(BatModels2[BatModels2$Betacov==0,PNames], na.rm = TRUE)
 
 # TOTAL RANK
 
-BatModels2[,'PropRank'] <- (BatModels2[,'PropRank'] > tvalues[1,'PropRank'])
+BatModels2[,'PropRank'] <- as.vector(BatModels2[,'PropRank'] > tvalues[1,'PropRank'])
 
 table(BatModels2[BatModels2$Betacov==0,'PropRank'])
 
@@ -52,19 +52,55 @@ BatModels2[grep('Rhinolophus',BatModels2$Sp),] %>%
 
 # Clean it up to write out
 
-BatModels2 %>% select(Sp, Betacov, P.Alb, P.Car3, P.Dal1, P.Far1, P.Gut1, P.Po2, P.Po3, PropRank) %>%
-  rename(Trait.3 = P.Alb,
+BatModels2 %>% select(Sp, Betacov, P.Alb, P.Car3, P.Dal1, P.Far1, P.Gut1, P.Po2, P.Po3, P.Stock1, PropRank) %>%
+  rename(Trait.1 = P.Gut1,
          Trait.2 = P.Car3,
-         Network.3 = P.Dal1,
-         Network.4 = P.Far1,
-         Trait.1 = P.Gut1,
+         Trait.3 = P.Alb,
          Network.1 = P.Po2,
          Network.2 = P.Po3,
-         Ensemble = PropRank) %>% write_csv("BinaryPredictions.csv")
+         Network.3 = P.Dal1,
+         Network.4 = P.Far1,
+         Hybrid.1 = P.Stock1,
+         Ensemble = PropRank) -> BatModels2 
+
+BatModels2 %>% write_csv("BinaryPredictions.csv")
 
 # WHO WON! WHO'S NEXT!
 
 verify <- c('Scotophilus kuhlii', 'Scotophilus heathii', 'Hipposideros larvatus', 'Hipposideros pomona', 'Pteropus lylei', 'Myotis pequinius', 'Myotis horsfieldii')
 
-BatModels2 %>% select(Sp, Betacov, P.Alb, P.Car3, P.Dal1, P.Far1, P.Gut1, P.Po2, P.Po3, PropRank) %>%
-  filter(Sp %in% verify)
+BatModels2 %>% filter(Sp %in% verify)
+
+#################################
+
+key1 <- c(`1` = "Reported", `0` = "Unreported")
+
+key2 <- c(`0` = "Unlikely", `1` = "Suspected",
+          `2` = "False -", `3` = "True +")
+
+BatModels2 %>% mutate_at(vars(contains(".")), ~(as.numeric(.))) %>%
+  mutate(Ensemble = as.numeric(Ensemble)) %>%
+  mutate(Betacov2 = Betacov*2) %>%
+  mutate_at(vars(contains(".")), ~(. + Betacov2)) %>% 
+  mutate(Ensemble = Ensemble + Betacov2) %>% 
+  mutate_at(vars(contains(".")), ~(recode(.,!!!key2))) %>% 
+  mutate(Ensemble =  recode(Ensemble,!!!key2)) %>% 
+  mutate(Betacov = recode(Betacov, !!!key1)) %>% 
+  mutate(`New data` = Betacov) %>% 
+  rename(`Training data` = Betacov) -> BatWeb
+
+BatWeb$`New data`[which(BatWeb$Sp %in% verify)] <- 'New data'
+
+BatWeb %>% select(c(`Training data`, 
+                    `New data`,
+                    Ensemble,
+                    Trait.1,
+                    Trait.2,
+                    Trait.3,
+                    Hybrid.1,
+                    Network.1,
+                    Network.2,
+                    Network.3,
+                    Network.4)) %>% rename(Hybrid = Hybrid.1) -> BatWeb
+
+BatWeb %>% write_csv("BinaryWebsite.csv")
